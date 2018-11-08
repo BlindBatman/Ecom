94b7877483b72f7fad99980503c9849bbb52931f
BlindBatman <34074590+BlindBatman@users.noreply.github.com>
2018-11-08 10:01:36 -0700

    Add project files.

---
+++ Commere_Aemlund/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Commere_Aemlund.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Catalog()
+        {
+            return View();
+        }
+
+        public ActionResult ProductDetails(string id)
+        {
+            int num = int.Parse(id);
+            return View(num);
+        }
+    }
+}
---
+++ Commere_Aemlund/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Commere_Aemlund.MvcApplication" Language="C#" %>
